a955f14d4928bfb34c6bdcbcaed9c105a0e33a16
ehsan <ehsan@ehsans-air.hsd1.tn.comcast.net>
2024-04-17 00:44:35 -0400

    Add Hooks class

---
+++ src/test/java/com/ehsan/utils/Hooks.java
@@ -0,0 +1,39 @@
+package com.ehsan.utils;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Hooks {
+
+	public static WebDriver driver;
+
+	public static void setUp() {
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	}
+
+	public static void tearDown() {
+
+		try {
+			Thread.sleep(3000);
+
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+
+		}
+		driver.quit();
+	}
+
+}
